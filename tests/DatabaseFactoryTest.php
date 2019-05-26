1ce2fd19666471ad41b99a9e0e12125030cb99c5
Robin de Graaf <hello@devvoh.com>
2019-05-26 10:47:00 +0000

    Update dependencies

--- tests/DatabaseFactoryTest.php
+++ tests/DatabaseFactoryTest.php
@@ -24,5 +24,27 @@ class DatabaseFactoryTest extends \PHPUnit\Framework\TestCase
         $database = $factory->createFromConfig($config);
 
         self::assertInstanceOf(Database::class, $database);
+        self::assertSame(Database::TYPE_MYSQL, $database->getType());
+        self::assertSame('localhost', $database->getHost());
+        self::assertSame('test_user', $database->getUsername());
+        self::assertSame('test_pass', $database->getPassword());
+        self::assertSame('test_database', $database->getDatabaseName());
+    }
+
+    public function testDatabaseFromSqliteData(): void
+    {
+        $config = new Config();
+        $config->set('parable.database', [
+            'type' => Database::TYPE_SQLITE,
+            'database' => 'test_database.sqlite',
+        ]);
+
+        $factory = new DatabaseFactory();
+
+        $database = $factory->createFromConfig($config);
+
+        self::assertInstanceOf(Database::class, $database);
+        self::assertSame(Database::TYPE_SQLITE, $database->getType());
+        self::assertSame('test_database.sqlite', $database->getDatabaseName());
     }
 }
